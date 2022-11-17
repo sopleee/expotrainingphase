a1694daf18331da5734ae34e2736523ed9adb451
sopleee <leesophie99@gmail.com>
2022-11-16 20:38:51 -0500

    modified lol

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TouchableOpacity, Image } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [topShow, setTopShow] = useState(false);
+  const [bottomShow, setBottomShow] = useState(false);
+  const changeCenter = (locale) => {
+    setTopShow(locale == 'Top');
+    setBottomShow(locale == 'Bottom');
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-white">
+      <TouchableOpacity onPress={() => changeCenter('Top')}>
+        <View style={{ backgroundColor: "#A9B0BA", width: 390, height: 340, alignItems: 'center', justifyContent: 'center' }}>
+          <Image source={{ uri: "https://www.aquablog.ca/wp-content/uploads/2016/05/Spot-prawn-main.jpg" }} style={{ width: 305, height: 159}} />
+        </View>
+      </TouchableOpacity>
+      {
+        !topShow && !bottomShow? (
+          <View className="items-center justify-center">
+            <Image source={{ uri: "https://www.oregonlive.com/resizer/sdwbSdFKcI0ht_0DqQ9NMQEJ9uM=/1280x0/smart/advancelocal-adapter-image-uploads.s3.amazonaws.com/expo.advance.net/img/bb90877c7a/width2048/11f_shark3.jpeg" }} style={{ width: 305, height: 159 }} />
+            <Text> neither </Text>
+          </View>
+        ) :
+        topShow ? (
+          <View className="items-center justify-center">
+            <Image source={{ uri: "https://www.mensjournal.com/wp-content/uploads/2014/03/CATERS_HAMMERHEAD_SHARKS_SURROUND_DIVER_FEEDING_TOUCHING_06.jpg?w=1200&h=1200&crop=1&quality=80&strip=all" }} style={{ width: 305, height: 159 }} />
+            <Text> yes top </Text>
+          </View>
+        ) :
+        bottomShow ? (
+          <View className="items-center justify-center">
+            <Image source={{ uri: "https://media.greenmatters.com/brand-img/c7EPLoIej/0x0/shark-1645644297742.jpg" }} style={{ width: 305, height: 159 }} />
+            <Text> yes bottom </Text>
+          </View>
+        ) 
+      :null}
+      <TouchableOpacity onPress={() => changeCenter('Bottom')}>
+      <View style={{ backgroundColor: "#1D2C1B", width: 390, height: 340, alignItems: 'center', justifyContent: 'center' }}>
+      <Image source={{ uri: "https://img.izismile.com/img/img3/20100606/640/the_fish_with_640_03.jpg" }} style={{ width: 305, height: 159 }} />
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
